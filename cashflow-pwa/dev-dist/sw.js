91c56fe599ba8ba83d6caa8834691440bd9e7516
Mzainulabidin72 <mzainulabidin72@gmail.com>
2026-09-13 14:30:25 +0700

    Upload semua file project

--- cashflow-pwa/dev-dist/sw.js
+++ cashflow-pwa/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-afac4cd2'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qq9viovbd78"
+    "revision": "0.hh1blnbj9s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
